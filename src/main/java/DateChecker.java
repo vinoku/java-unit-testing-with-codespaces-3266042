6f5ca8a46b1299cade91331ce8d3bccd38fc4676
vinoku <vinoku78@gmail.com>
2024-01-17 20:56:49 +0100

    solution

--- src/main/java/DateChecker.java
+++ src/main/java/DateChecker.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class DateChecker {
 
@@ -18,7 +19,8 @@ public class DateChecker {
     }
 
     public boolean has31Days(Month month) {
-        return false;
+        List<Month> monthWith31days = Arrays.asList(Month.JANUARY, Month.MARCH, Month.MAY, Month.JULY, Month.AUGUST, Month.OCTOBER, Month.DECEMBER);
+        return monthWith31days.contains(month);
     }
 
     
