9431ab91ff3daddb17ab5fe232e2d580af39dd38
vinoku <vinoku78@gmail.com>
2024-01-17 19:41:08 +0100

    solution

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Calculator {
 
     public int add(int x, int y) {
@@ -6,18 +8,28 @@ public class Calculator {
 
     // This method should return the result of subtracting y from x.
     public int subtract(int x, int y) {
-        return 0;
+        return x - y;
     }
 
     // This method should return the result of multiplying x and y.
     public int multiply(int x, int y) {
-        return 0;
+        return x * y;
     }
 
     // This method should return the result of diving x by y.
     public int divide(int x, int y) {
-        return 0;
+        if (y == 0) {
+            throw new IllegalArgumentException("y must be greater than zero");
+        }
+        return x / y;
     }
 
 
+    public boolean isEven(int i) {
+        return i % 2 == 0;
+    }
+
+    public int[] incrementArray(int[] ints) {
+        return Arrays.stream(ints).map(i -> i + 1).toArray();
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -13,6 +13,59 @@ public class CalculatorTest {
         int actual = calculator.add(1, 2);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubstract() {
+        int expected = 3;
+
+        int actual = calculator.subtract(7, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 12;
+
+        int actual = calculator.multiply(3, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 3;
+
+        int actual = calculator.divide(12, 4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsEven() {
+        boolean actual = calculator.isEven(4);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void testIncrementArray() {
+        int[] expected = new int[]{ 1+1, 2+1, 3+1 };
+
+        int[] actual = calculator.incrementArray(new int[]{ 1, 2, 3 });
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0));
+
+        String expectedMessage = "y must be greater than zero";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
     }
 
 }
